283a6d19c7bbc571cb974027c03a55dfb39990cc
itsevgenii <smazanovich@icloud.com>
2022-11-23 16:39:07 +0000

    installed cors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors"
 import "dotenv/config";
 
 import sqlRoutes from "../backend/routes/sqlRoutes.js";
@@ -12,7 +13,7 @@ const PORT = process.env.PORT;
 const ELEPHANT = process.env.POSTGRES_CONNECTION_URL;
 
 app.use(morgan("dev"));
-
+app.use(cors("*"))
 app.use(express.static("../public"));
 
 app.use(express.json());
